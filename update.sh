e1ea7dc724b24d247bf83dd67168a039597a6ed2
Deval Shah <dvlshah@users.noreply.github.com>
2018-04-11 14:22:19 +0530

    Delete update.sh

--- update.sh
+++
@@ -1,7 +0,0 @@
-echo "# Ticket_Booking_System" >> README.md
-git init
-git add .
-git commit -m "commit"
-git remote add origin git@github.com:dvlshah/Ticket_Booking_System.git
-git push -f origin master
-
